db1c25f8a0e26ecaee62e00476e9600ef9f90e39
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2021-06-13 14:08:23 -0500

    Implementando el Navbar

--- src/Containers/Layout.jsx
+++ src/Containers/Layout.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 const Layout = ({ children }) => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       {children}
     </>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import "../css/header.css";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const header = () => {
   return (
     <div className="header">
-      <h1>Prematix Web</h1>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand as={Link} to="/dashboard">
+          Prematix Web
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">
+              Cuidado neonatal
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Acerca de mi neonato
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Ver mi neonato
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Preguntas frecuentes
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Contactar pediatra
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Acerca de
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            {/* <Nav.Link href="#deets">More deets</Nav.Link> */}
+            <Nav.Link eventKey={2} as={Link} to="/login">
+              Iniciar Sesión
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
