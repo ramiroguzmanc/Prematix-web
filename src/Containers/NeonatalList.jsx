3c76b7c9dbcf62766d80787c044ee3634541dc5e
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2021-06-18 16:26:27 -0500

    Agregando pantalla de carga

--- src/Containers/NeonatalList.jsx
+++ src/Containers/NeonatalList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Spinner } from "react-bootstrap";
 import NeoCard from "../components/NeoCard";
 import firebase from "firebase";
 import { AuthContext } from "../Auth";
@@ -8,48 +8,66 @@ const NeonatalList = () => {
   const { currentUser } = useContext(AuthContext);
   const displayName = currentUser.displayName;
   const [neonatos, setNeonatos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const db = firebase.firestore();
-    db.collection("users")
-      .doc(currentUser.email)
-      .collection("neonatos")
-      .onSnapshot((qsp) => {
-        const neonatos = [];
-        qsp.docs.forEach((doc) => {
-          const { IMC, PC, born, height, lastname, name, weight } = doc.data();
-          neonatos.push({
-            id: doc.id,
-            IMC,
-            PC,
-            born,
-            height,
-            lastname,
-            name,
-            weight,
+    try {
+      db.collection("users")
+        .doc(currentUser.email)
+        .collection("neonatos")
+        .onSnapshot((qsp) => {
+          const neonatos = [];
+          qsp.docs.forEach((doc) => {
+            const { IMC, PC, born, height, lastname, name, weight } =
+              doc.data();
+            neonatos.push({
+              id: doc.id,
+              IMC,
+              PC,
+              born,
+              height,
+              lastname,
+              name,
+              weight,
+            });
           });
+          setNeonatos(neonatos);
+          setIsLoading(false);
         });
-        setNeonatos(neonatos);
-      });
+    } catch (error) {
+      console.error(error);
+    }
   }, []);
 
   return (
     <Container className="my-4">
-      <h1 className="text-center" style={{ color: "whitesmoke" }}>
-        Neonatos de {displayName}
-      </h1>
-      <Row className="justify-content-md-center my-5">
-        {neonatos.map((neo) => (
-          <NeoCard
-            key={neo.id}
-            name={neo.name}
-            lastname={neo.lastname}
-            weight={neo.weight}
-            height={neo.height}
-            IMC={neo.IMC}
-          />
-        ))}
-      </Row>
+      {isLoading ? (
+        <Row className="justify-content-md-center my-5">
+          <Spinner animation="border" role="status" variant="light">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </Row>
+      ) : (
+        <>
+          <h1 className="text-center" style={{ color: "whitesmoke" }}>
+            Neonatos de {displayName}
+          </h1>
+          <Row className="justify-content-md-center my-5">
+            {neonatos.map((neo) => (
+              <NeoCard
+                key={neo.id}
+                name={neo.name}
+                lastname={neo.lastname}
+                weight={neo.weight}
+                height={neo.height}
+                IMC={neo.IMC}
+              />
+            ))}
+          </Row>
+        </>
+      )}
     </Container>
   );
 };
